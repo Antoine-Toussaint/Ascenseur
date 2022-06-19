01187ea7d691edbe1caebffa769c03bd5c767bb3
x0000000001 <antoine.toussaint@depinfonancy.fr>
2022-06-19 19:34:45 +0200

    imported headers and main + cded person.c

---
+++ person.c
@@ -0,0 +1,20 @@
+#include "person.h"
+#include <stdlib.h>
+
+Person* createPerson(int src, int dest)
+{
+    Person *p = malloc(sizeof(Person));
+     p->src = src;
+     p->dest = dest;
+
+     return p;
+}
+
+PersonList *insert(Person *p, PersonList *list)
+{
+    PersonList * pl = malloc(sizeof(PersonList));
+    pl->person = p;
+    pl->next = list;
+
+    return pl;
+}
